bdc6276a66464b80b8c6a4f5f413bf866da01e07
Fathur Rohim <trustoim@gmail.com>
2024-11-25 15:07:06 +0700

    vertical and horizontal arrangement

--- UnitConverter/app/src/main/java/com/oimtrust/unitconverter/MainActivity.kt
+++ UnitConverter/app/src/main/java/com/oimtrust/unitconverter/MainActivity.kt
@@ -1,24 +1,25 @@
 package com.oimtrust.unitconverter
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.oimtrust.unitconverter.ui.theme.UnitConverterTheme
 
@@ -40,17 +41,28 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun UnitConverter() {
-    Column {
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
         // Here all the UI elements will be stacked below each other
         Text("Unit Converter")
         OutlinedTextField(value = "", onValueChange = {})
         Row {
-            val context = LocalContext.current
-            Button(onClick = {
-                Toast.makeText(context,
-                    "Thanks for clicking!",
-                    Toast.LENGTH_LONG).show() }) {
-                Text("Click Me!")
+            Box {
+                Button(onClick = {}) {
+                    Text("Select")
+                    Icon(Icons.Default.ArrowDropDown,
+                        contentDescription = "Arrow Down")
+                }
+            }
+            Box {
+                Button(onClick = {}) {
+                    Text("Select")
+                    Icon(Icons.Default.ArrowDropDown,
+                        contentDescription = "Arrow Down")
+                }
             }
         }
         Text("Result:")
